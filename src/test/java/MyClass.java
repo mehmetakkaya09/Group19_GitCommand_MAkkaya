198e29dfa9d4b3bf7327bef940304e5884ef6032
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2023-03-31 17:39:49 +0300

    second class is added

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
     public static void main(String[] args) {
 
         System.out.println("my first work is done");
+        System.out.println("my second work is done");
 
 
     }
